88f523db42c3b850f53d5fa76a44cf74ffcd8d6d
Andrei Storozhko <andrei_storozhko@epam.com>
2020-12-10 23:47:23 +0300

    feat: integrated with bff service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 
 const API_PATHS = {
-  product: 'https://x9ktsvx5ba.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://grx8ska1fd.execute-api.eu-west-1.amazonaws.com/dev', // proxy url to redirect to http for bff
   order: 'https://x9ktsvx5ba.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://ctgkhw46g9.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://x9ktsvx5ba.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://grx8ska1fd.execute-api.eu-west-1.amazonaws.com/dev', // proxy url to redirect to http
+  bff: 'https://grx8ska1fd.execute-api.eu-west-1.amazonaws.com/dev', // proxy url to redirect to http for bff
+  cart: 'https://grx8ska1fd.execute-api.eu-west-1.amazonaws.com/dev', // proxy url to redirect to http for bff
 };
 
 export default API_PATHS;
